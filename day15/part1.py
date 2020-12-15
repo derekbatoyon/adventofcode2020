0aca9a6d20d6c59aab270c12f01e9107b289a191
Derek Batoyon <derek@bytemeister.com>
2020-12-14 21:54:13 -0800

    Day 15

---
+++ day15/part1.py
@@ -0,0 +1,23 @@
+import sys
+
+def play(arg, until=2020):
+    numbers = [int(n) for n in arg.split(',')]
+    turn = len(numbers)
+
+    last = numbers.pop(-1)
+    memory = {n: i+1 for i, n in enumerate(numbers)}
+
+    while turn < until:
+        if last in memory:
+            speak = turn - memory[last]
+        else:
+            speak = 0
+        memory[last] = turn
+        turn += 1
+        last = speak
+
+    print(last)
+
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        play(arg)
